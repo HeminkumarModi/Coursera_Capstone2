50c30cfa83f9768cf96c414a52e6c3795d7d7955
HMODI <HMODI@HMODI-IN.in.oracle.com>
2019-08-10 20:01:54 +0530

    assignment_1

---
+++ Coursera_Capstone_assignment_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# My First capstone project
+
+# In[2]:
+
+
+import pandas as pd 
+import numpy as np
+
+
+# In[7]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
